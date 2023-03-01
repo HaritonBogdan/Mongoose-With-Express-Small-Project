5ac8f4edc6172f4b84c4578519e4b8b5602e16c7
Hariton Bogdan <hariton.bogdan11@gmail.com>
2023-03-01 20:48:49 +0200

    Mongo Relationships With Express

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override');
 const AppError = require("./AppError");
 
 const Product = require('./models/product');
+const Farm = require('./models/farm');
 const ObjectID = require('mongoose').Types.ObjectId;
 
 main().catch(err => console.log(err));
@@ -27,6 +28,63 @@ function wrapAsync(fn) {
         fn(req, res, next).catch(e => next(e));
     }
 }
+
+// FARM ROUTES
+
+app.get('/farms', wrapAsync(async (req, res, next) => {
+    const farms = await Farm.find({});
+    res.render('farms/index', { farms })
+}))
+
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new')
+})
+
+
+app.post('/farms', wrapAsync(async (req, res, next) => {
+    const farm = new Farm(req.body);
+    await farm.save()
+    res.redirect('/farms')
+}))
+
+app.get('/farms/:id', wrapAsync(async (req, res, next) => {
+    const farm = await Farm.findById(req.params.id).populate('products');
+    // console.log(farm);
+    res.render('farms/show', { farm })
+}))
+
+app.delete('/farms/:id', wrapAsync(async (req, res, next) => {
+    // console.log('delete');
+    const farm = await Farm.findByIdAndDelete(req.params.id);
+    res.redirect('/farms');
+}))
+
+app.get('/farms/:id/products/new', wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm })
+}))
+
+app.post('/farms/:id/products', wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${id}`)
+}))
+
+
+
+
+
+
+
+// PRODUCT ROUTES
+
 
 app.get('/products', wrapAsync(async (req, res, next) => {
     const { category } = req.query;
@@ -55,7 +113,8 @@ app.get('/products/:id', wrapAsync(async (req, res, next) => {
     if (!ObjectID.isValid(id)) {
         throw new AppError('Invalid Id', 400);
     }
-    const product = await Product.findById(id)
+    const product = await Product.findById(id).populate('farm', 'name');
+    console.log(product);
     if (!product) {
         throw new AppError('Product Not Found', 404);
     }
--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const productSchema = new mongoose.Schema({
+
+const productSchema = new Schema({
     name: {
         type: String,
         required: [true, 'name cannot be blank']
@@ -14,7 +16,11 @@ const productSchema = new mongoose.Schema({
         type: String,
         lowercase: true,
         enum: ['fruit', 'vegetable', 'dairy']
-    }
+    },
+    farm: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Farm'
+    }]
 })
 
 const Product = mongoose.model('Product', productSchema);
